0d790a2d29dc4566c4f38f8e2c698b195fff0c38
benz <ewwaldau1@pm.me>
2025-03-16 22:28:51 -0400

    Update example.lua
    Change wait to task.wait

--- example.lua
+++ example.lua
@@ -5,13 +5,13 @@ Notification:Notify(
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
 )
-wait(1)
+task.wait(1)
 Notification:Notify(
     {Title = "TEXT TITLE", Description = "DESCRIPTION"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
-wait(1)
+task.wait(1)
 Notification:Notify(
     {Title = "TEXT TITLE", Description = "DESCRIPTION"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
