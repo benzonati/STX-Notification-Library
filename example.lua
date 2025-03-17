d6d6c90c0c85a757500fd2ffb264a64b28b6d81c
benz <ewwaldau1@pm.me>
2025-03-16 22:28:08 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,1 +1,18 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/benzonati/STX-Notification-Library/refs/heads/main/main.lua"))()
 
+Notification:Notify(
+    {Title = "TEXT TITLE", Description = "DESCRIPTION"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+)
+wait(1)
+Notification:Notify(
+    {Title = "TEXT TITLE", Description = "DESCRIPTION"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+wait(1)
+Notification:Notify(
+    {Title = "TEXT TITLE", Description = "DESCRIPTION"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
+)
